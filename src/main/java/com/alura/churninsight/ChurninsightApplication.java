7ddaeda45619efd255a02545df228c65d1b0b1e0
pedro8734 <pedromontesprmd@gmail.com>
2025-12-20 15:33:11 -0500

    Initial commit

---
+++ src/main/java/com/alura/churninsight/ChurninsightApplication.java
@@ -0,0 +1,13 @@
+package com.alura.churninsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurninsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurninsightApplication.class, args);
+	}
+
+}
